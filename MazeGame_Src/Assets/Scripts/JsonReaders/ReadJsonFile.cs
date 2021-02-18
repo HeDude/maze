d971d4bacf8103020b0aa2cca391e394a59b5026
GermanScientist <jack_hulspas@hotmail.com>
2021-02-18 20:35:01 +0100

    Identify scpecific escaperoom start (Get question based on room)

--- MazeGame_Src/Assets/Scripts/JsonReaders/ReadJsonFile.cs
+++ MazeGame_Src/Assets/Scripts/JsonReaders/ReadJsonFile.cs
@@ -6,18 +6,30 @@ using LitJson;
 
 public class ReadJsonFile : MonoBehaviour
 {
-    private string rawJsonData;
+    //Data for the escape room
+    private string rawEscaperoomData;
     private JsonData escapeRoomData;
+
+    //Data for the questions
+    private string rawQuestionData;
+    private JsonData questionData;
 
     //Start is called before the first frame update
     private void Start()
     {
         //Read the json file (The raw data from the json file)
-        rawJsonData = File.ReadAllText(Application.dataPath + "/JsonFiles/escaperooms.json");
+        rawEscaperoomData = File.ReadAllText(Application.dataPath + "/JsonFiles/escaperooms.json");
 
         //This holds the Json object that we need to parse out into string text
         //The JsonMapper maps the raw text to a dictionary, so we can read it like any other dictionary
-        escapeRoomData = JsonMapper.ToObject(rawJsonData);
+        escapeRoomData = JsonMapper.ToObject(rawEscaperoomData);
+
+        //Read the json file (The raw data from the json file)
+        rawQuestionData = File.ReadAllText(Application.dataPath + "/JsonFiles/questions.json");
+
+        //This holds the Json object that we need to parse out into string text
+        //The JsonMapper maps the raw text to a dictionary, so we can read it like any other dictionary
+        questionData = JsonMapper.ToObject(rawQuestionData);
     }
 
     //Checks for collision for a trigger collider
@@ -33,7 +45,8 @@ public class ReadJsonFile : MonoBehaviour
             if (roomInfo != null)
             {
                 //Debugs the name of the escape room depending on its position
-                Debug.Log(escapeRoomData["escaperooms"][roomInfo.roomPosition]["name"]);
+                Debug.Log(escapeRoomData["escaperooms"][roomInfo.getRoomIndex()]["name"]);
+                Debug.Log(questionData["Questions"][roomInfo.getRoomIndex()]["Reproductive"]);
             }
         }
     }
--- MazeGame_Src/Assets/Scripts/JsonReaders/RoomInfo.cs
+++ MazeGame_Src/Assets/Scripts/JsonReaders/RoomInfo.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class RoomInfo : MonoBehaviour
 {
-    public string roomPosition;
+    [SerializeField] private string roomPosition;
+    private int roomIndex;
+
+    private void Start()
+    {
+        switch (roomPosition)
+        {
+            case "center": roomIndex = 0; break;
+            case "left": roomIndex = 1; break;
+            case "right": roomIndex = 2; break;
+            case "top": roomIndex = 3; break;
+            case "top-left": roomIndex = 4; break;
+            case "top-right": roomIndex = 5; break;
+            case "bottom": roomIndex = 6; break;
+            case "bottom-left": roomIndex = 7; break;
+            case "bottom-right": roomIndex = 8; break;
+            default: roomIndex = -1; break;
+        }
+    }
+
+    public int getRoomIndex()
+    {
+        return roomIndex;
+    }
 }
