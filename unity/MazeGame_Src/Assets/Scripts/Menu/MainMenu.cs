20fd9692a73c5f55cf8601e48c3ce8a919381466
GermanScientist <66174242+GermanScientist@users.noreply.github.com>
2021-04-15 01:39:47 +0200

    Added main menu

---
+++ unity/MazeGame_Src/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Runs when the button has been pressed
+    public void PressButton(string _buttonType)
+    {
+        switch (_buttonType)
+        {
+            case "play":
+                SceneManager.LoadScene("Maze");
+                break;
+            case "credits":
+                Debug.Log("start");
+                break;
+            case "quit":
+                Application.Quit();
+                break;
+            default:
+                Debug.Log("nothin");
+                break;
+        }
+    }
+}
